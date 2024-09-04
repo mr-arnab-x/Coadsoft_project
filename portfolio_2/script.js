48c6e0a309fd2be33f740dbe4472733b671850ee
mr-arnab-x <santraarnab241@gmail.com>
2024-09-05 01:14:17 +0530

    Initial Commit

---
+++ portfolio_2/script.js
@@ -0,0 +1,12 @@
+// Smooth scrolling for navigation
+document.querySelectorAll('nav ul li a').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+// Optionally add more JavaScript for animations or interactivity here
